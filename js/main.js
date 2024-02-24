be1bdafa3e268d7ce3cdb10a2c64c73f9aabb371
BOUNTY707 <mehrobakooo@gmail.com>
2024-02-25 00:28:46 +0500

    1ta page tugadi

--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@ var homeSlide = new Swiper(".homeSlide", {
         clickable: true
     },
 });
+
+// Catalog slide
+var catalogSlide = new Swiper(".catalogSlide", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
